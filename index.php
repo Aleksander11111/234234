58fc8b70936822b6b5246b5518a0bc0b912369e6
Alexander11111 <fcuktoreal@ukr.net>
2022-08-25 19:42:42 +0300

    lab2.2

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php echo strftime ('Сегодня %d-%m-%Y'); ?>
+<?php
+  // Установка локали и выбор значений даты
+  setlocale(LC_ALL, "russian");
+  $day = strftime('%d');
+  $mon = strftime('%B');
+  $mon = iconv("windows-1251", "UTF-8", $mon);
+  $year = strftime('%Y');
+  echo 'Сегодня ' , $day, ' число,', $mon, ' месяц,', $year, ' год.';
