ed78ef4607dd1c0c7e55c3cf918d29bda3aad402
Alexander11111 <fcuktoreal@ukr.net>
2022-09-01 16:42:10 +0300

    lab 2.4

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
+/*
+ * Получаем текущий час в виде строки от 00 до 23
+ * и приводим строку к целому числу от 0 до 23
+ */
+$hour = (int)strftime('%H');
+$welcome = ""; // Инициализируем переменную для приветствия
+  if ($hour > 0 and $hour < 6)
+      $welcome = "Доброй ночи";
+  elseif ($hour >= 6 and $hour < 12)
+    $welcome = "Доброе утро";
+  elseif ($hour >= 12 and $hour < 18)
+    $welcome = "Добрый день";
+  elseif ($hour >= 18 and $hour < 23)
+    $welcome = "Добрый вечер";
+  else
+      $welcome = "Доброй ночи";
+?>
+ // тут текст HTML, по этому открываю PHP(< ?)
+<?= $welcome ?>, Гость!
+ // и тут HTML
+<?php
   // Установка локали и выбор значений даты
   setlocale(LC_ALL, "russian");
   $day = strftime('%d');
@@ -7,3 +28,4 @@
   $year = strftime('%Y');
 
   echo "Сегодня  $day  число, $mon  месяц, $year  год.";
+  ?>
